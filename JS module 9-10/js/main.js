4c34b42e4c1fe1acfac5885b694c8a3e30890deb
persikov.igor <persikov-igor@mail.ru>
2016-05-19 22:26:55 +0300

    добавил модуль 9-10

--- JS module 9-10/js/main.js
+++ JS module 9-10/js/main.js
@@ -17,6 +17,23 @@ $(function () {
     // custom checkbox
     $(".custom-checkbox-js input[type='radio'], .custom-checkbox-js input[type='checkbox']").ionCheckRadio();
 
+    // Выпадающая меню
+    var $navLi = $('nav li');
+
+    // * добавляем стрелочки
+    $navLi.find('ul').eq(0).siblings('a').addClass('arrow-down');
+    $('.sub-menu').find('ul').siblings('a').addClass('arrow');
+
+    
+    $navLi.hover(function () {
+        if ($(this).find('ul').eq(0)) {
+            $(this).find('ul').eq(0).slideDown(200)
+        }
+    }, function () {
+        if ($(this).find('ul').eq(0)) {
+            $(this).find('ul').eq(0).slideUp(200)
+        }
+    });
 
 });//ready;
 
