ebaf101d671187e38a5fbd6e6d890e4274fc27dd
persikov.igor <persikov-igor@mail.ru>
2016-05-25 18:04:12 +0300

    рефакторинг тултипов

--- JS module 7-8/js/main.js
+++ JS module 7-8/js/main.js
@@ -13,75 +13,22 @@ $(function () {
     });
 
     //*********** FORM **************
-    var $inputFirsName = $('.form__first-name');
-    var $inputLastName = $('.form__last-name');
-    var $inputAddress = $('.form__address');
+    var $input = $('.form input');
 
-    var $help1 = $('.form__help-1');
-    var $help2 = $('.form__help-2');
-    var $help3 = $('.form__help-3');
+    $input.hover(function () {
+        $(this).next().show(200);
 
-    // Обработчик для input
-    $inputFirsName.hover(function (e) {
-        var target = e.target.className;
-        target = '.' + target;
-
-        if (target == $inputFirsName.selector) {
-            $help1.show(200);
-        }
-
-    }, function (e) {
-        var target = e.target.className;
-        target = '.' + target;
-
-        if (target == $inputFirsName.selector) {
-            $help1.hide(200);
-        }
+    }, function () {
+        $(this).next().hide(200);
     });
 
-    // Обработчик для input
-    $inputLastName.hover(function (e) {
-        var target = e.target.className;
-        target = '.' + target;
-
-        if (target == $inputLastName.selector) {
-            $help2.show(200);
-        }
-
-    }, function (e) {
-        var target = e.target.className;
-        target = '.' + target;
-
-        if (target == $inputLastName.selector) {
-            $help2.hide(200);
-        }
-    });
-
-    // Обработчик для input
-    $inputAddress.hover(function (e) {
-        var target = e.target.className;
-        target = '.' + target;
-
-        if (target == $inputAddress.selector) {
-            $help3.show(200);
-        }
-
-    }, function (e) {
-        var target = e.target.className;
-        target = '.' + target;
-
-        if (target == $inputAddress.selector) {
-            $help3.hide(200);
-        }
-    });
 
     var btnHelp = $('.form__btn-help');
 
     // Обработчик для кнопки
     btnHelp.on('click', function () {
-        $help1.show(200);
-        $help2.show(200);
-        $help3.show(200);
+        $input.next().show(200);
 
+        return false
     });
 });//ready;
