5e90b3c6b987d8db42fe5d0a050832f316f3b8da
persikov.igor <persikov-igor@mail.ru>
2016-07-07 00:26:29 +0300

    скрипт и css готово остался requeryJS

--- JS module 23-24/js/script.js
+++ JS module 23-24/js/script.js
@@ -4,12 +4,13 @@ function Model () {
 	self.data = ['test 1','test 2','test 3'];
 
 	self.addItem = function (item) {
-		if (item.length === 0) {
+		if (item.length === 0 || item === ' ') {
 			return;
 		}
 
 		self.data.push(item);
 	};
+
 	self.removeItem = function (item) {
 		var index = self.data.indexOf(item);
 
@@ -18,6 +19,12 @@ function Model () {
 		}
 
 		self.data.splice(index, 1);
+	};
+
+	self.editItem = function (currentItem, editedItem) {
+		var index = self.data.indexOf(currentItem);
+
+		self.data.splice(index, 1, editedItem);
 	};
 }
 
@@ -41,19 +48,60 @@ function View (model) {
 		self.elements.listContainer.html(content);
 	};
 
+	self.showEditor = function showEditor () {
+		$('.todo-list__edit').hide();
+		$(this).next().show()
+				.find('input').focus();
+	};
+
 	init();
+
+	self.showEditor = function (that) {
+		$('.todo-list__edit').hide();
+		$(that).next().show()
+				.find('input').focus();
+	}
 }
 
 function Controller (model, view) {
 	view.elements.addBtn.on('click', addItem);
 	view.elements.listContainer.on('click', '.todo-list__delete', removeItem);
+	view.elements.listContainer.on('dblclick', '.todo-list__item', showEditor);
+	view.elements.listContainer.on('click', '.todo-list__btn-edit', editItem);
+	$('html').on('click', closeEditor);
 
-	function addItem () {
+	var currentItem;
+
+	function editItem (e) {
+		var editedItem = $(this).siblings('.todo-list__field-edit').val();
+
+		model.editItem(currentItem, editedItem);
+		view.renderList(model.data);
+		$('.todo-list__edit').hide();
+
+		e.preventDefault();
+	}
+
+	function showEditor () {
+		currentItem = $(this).find('span').attr('data-value');
+
+		view.showEditor(this);
+	}
+
+	function closeEditor () {
+		if (this !== $('.todo-list__edit')) {
+			$('.todo-list__edit').hide();
+		}
+	}
+
+	function addItem (e) {
 		var newItem = view.elements.input.val();
 
 		model.addItem(newItem);
 		view.renderList(model.data);
 		view.elements.input.val('');
+
+		e.preventDefault();
 	}
 
 	function removeItem () {
@@ -69,9 +117,6 @@ $(function () {
 	var model = new Model();
 	var view = new View(model);
 	var controller = new Controller(model, view);
-
-	//console.log(view);
-
 
 
 });// Ready
