8e6f0a1a4c48fff0b7d512306d8e1c6d744a42f0
persikov.igor <persikov-igor@mail.ru>
2016-07-19 17:59:01 +0300

    the mobile version is ready

---
+++ JS final exam/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp          = require('gulp'),
+	 autoprefixer  = require('gulp-autoprefixer'),
+	 sass          = require('gulp-sass'),
+	 browserSync   = require('browser-sync'),
+	 sourcemaps    = require('gulp-sourcemaps');
+
+
+gulp.task('sass', function() {
+	return gulp.src('sass/*.scss')
+		 .pipe(sourcemaps.init())
+		 .pipe(sass())
+		 .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7']))
+		 .pipe(sourcemaps.write('./'))
+		 .pipe(gulp.dest('css/'))
+		 .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('html', function() {
+	return gulp.src('./index.html')
+		 .pipe(gulp.dest('./'))
+		 .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js', function() {
+	return gulp.src('js/*.js')
+		 .pipe(gulp.dest('js/'))
+		 .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('webServer', function() {
+	browserSync({
+		server: {
+			baseDir: './'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.scss', ['webServer', 'sass']);
+	gulp.watch('./index.html', ['webServer', 'html']);
+	gulp.watch('js/*.js', ['webServer', 'js']);
+});
+
---
+++ JS final exam/js/script.js
@@ -0,0 +1,38 @@
+$(function () {
+
+	var searchField = $('.search-partner__field');
+	var searchBtn = $('.search-partner__btn');
+	var tmplHtml = $('#tmplHtml').html();
+
+	function ajaxQuery(query) {
+		$.ajax({
+			method: 'GET',
+			url: 'https://pixabay.com/api/',
+			data: 'key=2701940-50686df15687ed4665dfca084&q=' + query + '&image_type=photo&lang=ru,en&per_page=7',
+			success: function (obj) {
+				for (var i = 0; i < obj.hits.length; i++) {
+					var objImages = obj.hits[i];
+					var content = tmpl(tmplHtml, objImages);
+
+					$('.tiles').append(content);
+				}
+			}
+		});
+	}
+
+	ajaxQuery('travel');
+
+	searchBtn.click(function (e) {
+		var query = searchField.val();
+
+		searchField.val('');
+		searchField.blur();
+		$('.tiles').html('');
+
+		ajaxQuery(query);
+
+		e.preventDefault();
+	});
+
+
+});//Ready
